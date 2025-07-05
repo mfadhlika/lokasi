3dfb7cb4f7c6d1c18200141e0d177d419b3354e5
Muhammad Fadhlika <git@fadhlika.com>
2025-07-05 23:12:40 +0700

    fix location query

--- src/main/java/com/fadhlika/lokasi/repository/LocationRepository.java
+++ src/main/java/com/fadhlika/lokasi/repository/LocationRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.fadhlika.lokasi.model.Location;
+
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 import org.slf4j.Logger;
@@ -21,6 +22,10 @@ import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author fadhl
@@ -50,11 +55,11 @@ public class LocationRepository {
             location.setSpeed(rs.getDouble("speed"));
             location.setAccuracy(rs.getInt("accuracy"));
             location.setVerticalAccuracy(rs.getInt("vertical_accuracy"));
-//            location.setMotions(rs.getString("motions"));
             location.setBatteryState(rs.getInt("battery_state"));
             location.setBattery(rs.getDouble("battery"));
             location.setSsid(rs.getString("ssid"));
             location.setTimestamp(LocalDateTime.parse(rs.getString("timestamp")));
+            location.setCourseAccuracy(rs.getInt("course_accuracy"));
             location.setCreatedAt(LocalDateTime.parse(rs.getString("created_at")));
             String pointWkt = rs.getString("geometry");
             if (pointWkt != null) {
@@ -64,75 +69,86 @@ public class LocationRepository {
                     throw new RuntimeException(e);
                 }
             }
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                location.setMotions(mapper.readValue(rs.getString("motions"), new TypeReference<List<String>>() {
+                }));
+            } catch (JsonProcessingException e) {
+                // DO NOTHING
+            }
             return location;
         }
     };
 
     public void createLocation(Location location) {
-        createLocations(new ArrayList<>() {{
-            add(location);
-        }});
+        createLocations(new ArrayList<>() {
+            {
+                add(location);
+            }
+        });
     }
 
     public void createLocations(List<Location> locations) {
         try {
             jdbcTemplate.batchUpdate(
-                    "INSERT INTO location(" +
-                            "user_id, " +
-                            "device_id, " +
-                            "geometry, " +
-                            "altitude, " +
-                            "course, " +
-                            "course_accuracy, " +
-                            "speed, " +
-                            "accuracy, " +
-                            "vertical_accuracy, " +
-                            "motions, " +
-                            "battery_state, " +
-                            "battery, " +
-                            "ssid, " +
-                            "timestamp, " +
-                            "raw_data, " +
-                            "created_at, " +
-                            "import_id) VALUES(?, ?, ST_GeomFromText(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO location("
+                    + "user_id, "
+                    + "device_id, "
+                    + "geometry, "
+                    + "altitude, "
+                    + "course, "
+                    + "course_accuracy, "
+                    + "speed, "
+                    + "accuracy, "
+                    + "vertical_accuracy, "
+                    + "IFNULL(motions, \"[]\") AS motions, "
+                    + "battery_state, "
+                    + "battery, "
+                    + "ssid, "
+                    + "timestamp, "
+                    + "raw_data, "
+                    + "created_at, "
+                    + "import_id) VALUES(?, ?, ST_GeomFromText(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     new BatchPreparedStatementSetter() {
-                        @Override
-                        public void setValues(PreparedStatement ps, int i) throws SQLException {
-                            Location location = locations.get(i);
-                            ps.setInt(1, location.getUserId());
-                            ps.setString(2, location.getDeviceId());
-                            ps.setObject(3, location.getGeometry());
-                            ps.setInt(4, location.getAltitude());
-                            ps.setInt(5, location.getCourse());
-                            ps.setInt(6, location.getCourseAccuracy());
-                            ps.setDouble(7, location.getSpeed());
-                            ps.setInt(8, location.getAccuracy());
-                            ps.setInt(9, location.getVerticalAccuracy());
-                            ps.setObject(10, location.getMotions());
-                            ps.setObject(11, location.getBatteryState());
-                            ps.setDouble(12, location.getBattery());
-                            ps.setString(13, location.getSsid());
-                            ps.setObject(14, location.getTimestamp());
-                            ps.setString(15, location.getRawData());
-                            ps.setObject(16, location.getCreatedAt());
-                            ps.setObject(17, location.getImportId());
-                        }
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    Location location = locations.get(i);
+                    ps.setInt(1, location.getUserId());
+                    ps.setString(2, location.getDeviceId());
+                    ps.setObject(3, location.getGeometry());
+                    ps.setInt(4, location.getAltitude());
+                    ps.setInt(5, location.getCourse());
+                    ps.setInt(6, location.getCourseAccuracy());
+                    ps.setDouble(7, location.getSpeed());
+                    ps.setInt(8, location.getAccuracy());
+                    ps.setInt(9, location.getVerticalAccuracy());
+                    ps.setObject(10, location.getMotions());
+                    ps.setObject(11, location.getBatteryState());
+                    ps.setDouble(12, location.getBattery());
+                    ps.setString(13, location.getSsid());
+                    ps.setObject(14, location.getTimestamp());
+                    ps.setString(15, location.getRawData());
+                    ps.setObject(16, location.getCreatedAt());
+                    ps.setObject(17, location.getImportId());
+                }
 
-                        @Override
-                        public int getBatchSize() {
-                            return locations.size();
-                        }
-                    });
+                @Override
+                public int getBatchSize() {
+                    return locations.size();
+                }
+            });
         } catch (DataAccessException e) {
             logger.error("error creating point: {}", e.getMessage(), e);
         }
     }
 
     public List<Location> findLocations(int userId, LocalDateTime start, LocalDateTime end, Optional<String> device) throws SQLException {
-        List<String> where = new ArrayList<>() {{
-            add("user_id = ?");
-            add("timestamp BETWEEN ? AND ?");
-        }};
+        List<String> where = new ArrayList<>() {
+            {
+                add("user_id = ?");
+                add("timestamp BETWEEN ? AND ?");
+            }
+        };
 
         List<Object> args = new ArrayList<Object>() {
             {
@@ -147,6 +163,25 @@ public class LocationRepository {
             args.add(d);
         });
 
-        return jdbcTemplate.query("SELECT * FROM location WHERE " + String.join(" AND ", where), locationRowMapper, args.toArray());
+        return jdbcTemplate.query("SELECT "
+                + "id, "
+                + "user_id, "
+                + "device_id, "
+                + "AsText(geometry) AS geometry, "
+                + "altitude, "
+                + "course, "
+                + "speed, "
+                + "accuracy, "
+                + "vertical_accuracy, "
+                + "motions, "
+                + "battery_state, "
+                + "battery, "
+                + "ssid, "
+                + "raw_data, "
+                + "timestamp, "
+                + "created_at, "
+                + "import_id, "
+                + "course_accuracy FROM location WHERE "
+                + String.join(" AND ", where), locationRowMapper, args.toArray());
     }
 }
