d65673c3f7c3a215232081f53f76d79f13550b17
Muhammad Fadhlika <git@fadhlika.com>
2025-07-14 14:46:07 +0700

    replace JdbcTemplate to JdbcClient in ImportRepository

--- src/main/java/com/fadhlika/lokasi/repository/ImportRepository.java
+++ src/main/java/com/fadhlika/lokasi/repository/ImportRepository.java
@@ -2,6 +2,7 @@ package com.fadhlika.lokasi.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import com.fadhlika.lokasi.model.Import;
@@ -9,25 +10,31 @@ import com.fadhlika.lokasi.model.Import;
 @Repository
 public class ImportRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcClient jdbcClient;
 
     @Autowired
     public ImportRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcClient = JdbcClient.create(jdbcTemplate);
     }
 
     public int saveImport(Import anImport) {
-        jdbcTemplate.update(
-                "INSERT INTO import(user_id, source, filename, path, content, checksum, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                anImport.userId(), anImport.source(), anImport.filename(), anImport.path(), anImport.content(), anImport.checksum());
-        return jdbcTemplate.queryForObject("SELECT last_insert_rowid()", Integer.class);
+        jdbcClient.sql(
+                "INSERT INTO import(user_id, source, filename, path, content, checksum, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)")
+                .param(anImport.userId())
+                .param(anImport.source())
+                .param(anImport.filename())
+                .param(anImport.path())
+                .param(anImport.content())
+                .param(anImport.checksum())
+                .update();
+        return jdbcClient.sql("SELECT last_insert_rowid()").query(Integer.class).single();
     }
 
     public void deleteImport(int importId) {
-        jdbcTemplate.update("DELETE FROM import WHERE id = ?", importId);
+        jdbcClient.sql("DELETE FROM import WHERE id = ?").param(importId).update();
     }
 
     public void updateImportStatus(int importId, boolean status) {
-        jdbcTemplate.update("UPDATE import SET done = ? WHERE id = ?", status, importId);
+        jdbcClient.sql("UPDATE import SET done = ? WHERE id = ?").param(status).param(importId).update();
     }
 }
