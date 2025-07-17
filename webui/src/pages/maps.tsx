002d698c585a6cf99375dd8c8537655a9c161740
Muhammad Fadhlika <git@fadhlika.com>
2025-07-17 12:01:25 +0700

    move location filter state to query params

--- webui/src/pages/maps.tsx
+++ webui/src/pages/maps.tsx
@@ -9,35 +9,19 @@ import { Maps } from "@/components/maps";
 import type { FeatureCollection, LineString } from "geojson";
 import { Header } from "@/components/header";
 import { toast } from "sonner";
+import { useLocationFilter } from "@/hooks/use-location-filter";
 
 export default function MapsPage() {
     const [locations, setLocations] = useState<FeatureCollection>({ type: 'FeatureCollection', features: [] } as FeatureCollection);
     const [position, setPosition] = useState<LatLngTuple>([-6.175, 106.8275]);
-    const [date, setDate] = useState<DateRange | undefined>(() => {
-        const start = new Date();
-        start.setHours(0, 0, 0, 0);
-
-        const end = new Date();
-        end.setHours(23, 59, 59, 59);
-
-        return {
-            from: start,
-            to: end,
-        };
-    });
-    const [device, setDevice] = useState<string>('all');
+    const [{ date, device }, setFilter] = useLocationFilter();
 
     useEffect(() => {
         const params = new URLSearchParams();
-        if (date?.from) {
-            params.set('start', date.from.toISOString());
-        }
-        if (date?.to) {
-            params.set('end', date.to.toISOString());
-        }
-        if (device !== 'all') {
-            params.set('device', device);
-        }
+        if (date?.from) params.set('start', date.from.toJSON());
+        if (date?.to) params.set('end', date.to.toJSON());
+        if (device && device != 'all') params.append('device', device);
+
         axiosInstance.get(`v1/locations?${params.toString()}`)
             .then((res) => {
                 const data = res.data as FeatureCollection & { message: string }
@@ -49,11 +33,25 @@ export default function MapsPage() {
             .catch(err => toast.error(`Failed to get user's locations: ${err}`));
     }, [date, device]);
 
+    const handleDate = (newDate: DateRange | undefined) => {
+        setFilter({
+            date: newDate,
+            device
+        });
+    }
+
+    const handleDevice = (newDevice: string) => {
+        setFilter({
+            date,
+            device: newDevice
+        });
+    }
+
     return (
         <>
             <Header>
-                <DatePicker variant="outline" date={date} setDate={setDate} />
-                <DeviceSelect className="" selectedDevice={device} onSelectedDevice={setDevice} />
+                <DatePicker variant="outline" date={date} setDate={handleDate} />
+                <DeviceSelect className="" selectedDevice={device || "all"} onSelectedDevice={handleDevice} />
             </Header>
             <div className="flex flex-1 flex-col gap-4">
                 <Maps position={position} locations={locations} />
