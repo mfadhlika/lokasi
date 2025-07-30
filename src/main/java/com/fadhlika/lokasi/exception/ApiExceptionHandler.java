07705dda57878043a0742b966e0e397e2c232374
Muhammad Fadhlika <git@fadhlika.com>
2025-07-30 10:09:28 +0700

    return httpstatus according to annotation in handleGenericException

--- src/main/java/com/fadhlika/lokasi/exception/ApiExceptionHandler.java
+++ src/main/java/com/fadhlika/lokasi/exception/ApiExceptionHandler.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.auth0.jwt.exceptions.SignatureVerificationException;
@@ -19,7 +21,12 @@ public class ApiExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Response> handleGenericException(RuntimeException ex) {
         logger.error("Exception handled: {}", ex.getMessage());
-        return ResponseEntity.internalServerError().body(new Response(ex.getMessage()));
+        BodyBuilder builder = ResponseEntity.internalServerError();
+        if (ex.getClass().isAnnotationPresent(ResponseStatus.class)) {
+            ResponseStatus status = ex.getClass().getAnnotation(ResponseStatus.class);
+            builder = ResponseEntity.status(status.value());
+        }
+        return builder.body(new Response(ex.getMessage()));
     }
 
     @ExceptionHandler(MissingRequestCookieException.class)
