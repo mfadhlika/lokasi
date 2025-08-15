c493c45bde5a0ccd7ef2478e32dc677c5e662b74
Muhammad Fadhlika <git@fadhlika.com>
2025-08-15 10:30:19 +0700

    add security filter exception

---
+++ src/main/java/com/fadhlika/lokasi/config/SecurityFilterException.java
@@ -0,0 +1,29 @@
+package com.fadhlika.lokasi.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilterException extends OncePerRequestFilter {
+    @Autowired
+    private HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
+        }
+    }
+}
