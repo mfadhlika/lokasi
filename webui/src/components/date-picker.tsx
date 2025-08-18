b786072f0f9da6c0a675a9e09692f2ee4f487d83
Muhammad Fadhlika <git@fadhlika.com>
2025-08-18 22:00:31 +0700

    modify date-picker to be not automatically save

--- webui/src/components/date-picker.tsx
+++ webui/src/components/date-picker.tsx
@@ -8,6 +8,7 @@ import { Popover, PopoverContent } from "./ui/popover";
 import { PopoverTrigger } from "@radix-ui/react-popover";
 import { CalendarIcon } from "lucide-react";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { useState } from "react";
 
 export type DatePickerProps = {
     className?: string,
@@ -21,10 +22,11 @@ export function DatePicker({
     setDate,
     variant
 }: DatePickerProps) {
+    const [tempDate, setTempDate] = useState<DateRange | undefined>(date);
     const selectDate = (value?: DateRange) => {
         value?.from?.setHours(0, 0, 0, 0);
         value?.to?.setHours(23, 59, 59, 59);
-        setDate(value);
+        setTempDate(value);
     }
 
     const formatDateRange = (range?: DateRange): string => {
@@ -33,58 +35,70 @@ export function DatePicker({
         return `${format(range.from!, "LLL dd, y")} - ${format(range.to!, "LLL dd, y")}`
     }
 
+    const saveDate = () => {
+        setDate(tempDate);
+    }
+
+    const onOpenChange = (open: boolean) => {
+        if (open) setTempDate(date);
+    }
+
     return (
-        <Popover>
+        <Popover onOpenChange={onOpenChange}>
             <PopoverTrigger asChild>
                 <Button id="date" variant={variant ?? "ghost"} className={cn(
                     "w-auto justify-start text-left font-normal",
-                    !date && "text-muted-foreground"
+                    !tempDate && "text-muted-foreground"
                 )}>
                     <CalendarIcon />
                     {formatDateRange(date)}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="p-0 z-100000" align="start" asChild>
-                <Card className="max-w-[300px] py-4">
+                <Card className="max-w-[300px] py-2">
                     <CardContent>
                         <Calendar
+                            className="pb-0"
                             autoFocus
                             mode="range"
-                            selected={date}
+                            selected={tempDate}
                             onSelect={selectDate}
                             captionLayout="dropdown"
                         />
                     </CardContent>
                     <CardFooter className="flex flex-wrap gap-2 border-t px-4 !pt-4">
-                        {[
-                            { label: "Today", value: 0 },
-                            { label: "Yesterday", value: 1 },
-                            { label: "Last week", value: 7 },
-                            { label: "Last 2 weeks", value: 14 },
-                            { label: "Last Month", value: 31 },
-                        ].map((preset) => (
-                            <Button
-                                key={preset.value}
-                                variant="outline"
-                                size="sm"
-                                className="flex-1"
-                                onClick={() => {
-                                    const todayDate = new Date();
-                                    todayDate.setHours(23, 59, 59, 59);
-                                    const newDate = subDays(todayDate, preset.value);
-                                    newDate.setHours(0, 0, 0, 0);
-                                    setDate({
-                                        from: newDate,
-                                        to: todayDate
-                                    });
-                                }}
-                            >
-                                {preset.label}
-                            </Button>
-                        ))}
+                        <div className="flex flex-wrap gap-2 mb-2">
+                            {[
+                                { label: "Today", value: 0 },
+                                { label: "Yesterday", value: 1 },
+                                { label: "Last week", value: 7 },
+                                { label: "Last 2 weeks", value: 14 },
+                                { label: "Last Month", value: 31 },
+                            ].map((preset) => (
+                                <Button
+                                    key={preset.value}
+                                    variant="outline"
+                                    size="sm"
+                                    className="flex-1"
+                                    onClick={() => {
+                                        const todayDate = new Date();
+                                        todayDate.setHours(23, 59, 59, 59);
+                                        const newDate = subDays(todayDate, preset.value);
+                                        newDate.setHours(0, 0, 0, 0);
+                                        setDate({
+                                            from: newDate,
+                                            to: todayDate
+                                        });
+                                    }}
+                                >
+                                    {preset.label}
+                                </Button>
+                            ))}
+                        </div>
+                        <Button className="w-full" onClick={saveDate}>Save</Button>
                     </CardFooter>
                 </Card>
             </PopoverContent>
-        </Popover>
+        </Popover >
     )
 }
