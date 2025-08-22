6769c7424c511ba95cdd87ca05a717609f70ae21
Muhammad Fadhlika <git@fadhlika.com>
2025-08-22 10:19:35 +0700

    set api proxy target in env

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -2,35 +2,40 @@
 /// <reference types="@vitest/browser/providers/playwright" />
 /// <reference types="@vitest/browser/matchers" />
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
+
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+    return {
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:8080'
-        }
-    },
-    test: {
-        include: ['**/*.spec.tsx'],
-        browser: {
-            enabled: true,
-            provider: 'playwright',
-            instances: [
-                {
-                    browser: 'chromium',
-                    headless: true
-                }
-            ]
+        server: {
+            proxy: {
+                '/api': env.VITE_API_BASE_URL
+            }
         },
-    },
+        test: {
+            include: ['**/*.spec.tsx'],
+            browser: {
+                enabled: true,
+                provider: 'playwright',
+                instances: [
+                    {
+                        browser: 'chromium',
+                        headless: true
+                    }
+                ]
+            },
+        },
+    };
 })
