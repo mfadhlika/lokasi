3d3fbae29e4f7243020dc954a87e39d7a5ce914c
Muhammad Fadhlika <git@fadhlika.com>
2025-08-23 11:07:51 +0700

    drop jobrunr tables

---
+++ src/main/resources/db/migration/V15__drop_jobrunr_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS jobrunr_backgroundjobservers;
+DROP TABLE IF EXISTS jobrunr_jobs;
+DROP VIEW IF EXISTS jobrunr_jobs_stats;
+DROP TABLE IF EXISTS jobrunr_metadata;
+DROP TABLE IF EXISTS jobrunr_migrations;
+DROP TABLE IF EXISTS jobrunr_recurring_jobs;
