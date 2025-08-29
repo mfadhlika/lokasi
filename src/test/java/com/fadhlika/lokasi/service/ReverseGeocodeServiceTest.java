a12006ee0cc3519a62787ac2ecdee2bc98204822
Muhammad Fadhlika <git@fadhlika.com>
2025-08-29 15:21:43 +0700

    update reverse geocode test

--- src/test/java/com/fadhlika/lokasi/service/ReverseGeocodeServiceTest.java
+++ src/test/java/com/fadhlika/lokasi/service/ReverseGeocodeServiceTest.java
@@ -1,10 +1,7 @@
 package com.fadhlika.lokasi.service;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,12 +18,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.fadhlika.lokasi.dto.Feature;
 import com.fadhlika.lokasi.dto.FeatureCollection;
+import com.fadhlika.lokasi.dto.GeocodeProperties;
 import com.fadhlika.lokasi.model.Location;
+import com.fadhlika.lokasi.model.Place;
 import com.fadhlika.lokasi.repository.LocationRepository;
 import com.fadhlika.lokasi.repository.PhotonRepository;
+import com.fadhlika.lokasi.repository.PlaceRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class ReverseGeocodeServiceTest {
@@ -40,28 +42,65 @@ public class ReverseGeocodeServiceTest {
     @Mock
     private PhotonRepository photonRepository;
 
+    @Mock
+    private PlaceRepository placeRepository;
+
+    @Mock
+    private PlatformTransactionManager transactionManager;
+
+    @Mock
+    private ObjectMapper mapper;
+
     @Test
     public void testReverseGeocodeLocation() throws IOException, InterruptedException {
+        Location l = new Location(-6.1754, 106.8272) {
+            {
+                setId(1);
+                setUserId(1);
+                setTimestamp(ZonedDateTime.now(ZoneOffset.UTC));
+            }
+        };
+
         when(locationRepository.findLocation(Optional.empty(), Optional.empty(),
                 Optional.empty(), Optional.empty(),
                 Optional.empty(), Optional.empty(), Optional.of(false)))
-                .thenReturn(Optional.of(new Location(-6.1754, 106.8272) {
-                    {
-                        setId(1);
-                        setUserId(1);
-                        setTimestamp(ZonedDateTime.now(ZoneOffset.UTC));
-                    }
-                }))
+                .thenReturn(Optional.of(l))
                 .thenReturn(Optional.empty());
 
-        when(photonRepository.reverseGeocode(-6.1754, 106., 3)).thenReturn(new FeatureCollection(new ArrayList<>() {
+        when(photonRepository.reverseGeocode(-6.1754, 106.8272, 3)).thenReturn(new FeatureCollection(new ArrayList<>() {
             {
-                add(new Feature());
+                add(new Feature(l.getGeometry(), new GeocodeProperties(
+                        "W",
+                        1159084257,
+                        "historic",
+                        "monument",
+                        "house",
+                        "10110",
+                        "ID",
+                        "Monumen Nasional",
+                        "Indonesia",
+                        "Jakarta",
+                        "Gambir",
+                        "RW 02",
+                        "Jalan Medan Merdeka Utara",
+                        "Jawa",
+                        new ArrayList<>() {
+                            {
+                                add(106.8267455);
+                                add(-6.1749898);
+                                add(106.8275888);
+                                add(-6.1758248);
+                            }
+                        })));
             }
         }));
 
         doNothing().when(locationRepository).updateLocationGeocode(eq(1),
                 any(FeatureCollection.class));
+
+        when(placeRepository.fetchPlace(l.getGeometry())).thenReturn(Optional.empty());
+
+        doNothing().when(placeRepository).createPlace(any(Place.class));
 
         reverseGeocodeService.processReverseGeocode();
 
@@ -71,6 +110,10 @@ public class ReverseGeocodeServiceTest {
                 Optional.empty(),
                 Optional.empty(), Optional.empty(),
                 Optional.empty(), Optional.empty(), Optional.of(false));
+
+        verify(placeRepository, times(1)).fetchPlace(l.getGeometry());
+
+        verify(placeRepository, times(1)).createPlace(any(Place.class));
 
     }
 }
